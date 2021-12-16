973e0762bf6a9d7ef3bef015c40df616ce6bcb27
Patrick Di Fazio <50186694+BlessedRebuS@users.noreply.github.com>
2021-12-16 16:03:35 +0100

    Add files via upload

--- TemplatesLogin/src/bean/Dati.java
+++ TemplatesLogin/src/bean/Dati.java
@@ -3,36 +3,49 @@ package bean;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 import javax.servlet.http.HttpSession;
 
 public class Dati {
 
-    private Map<String, String> map;
-    private Map<String, HttpSession> sessions;
+    private Map<String, String> users;
+    private Map<String, Optional<HttpSession>> sessions;
+    private Map<String, String> groups;
 
 public Dati(){
 
     //login
-    map = new HashMap<>();
-    map.put("admin", "admin");
-    map.put("utente1", "utente1");
-    map.put("utente2", "utente2");
-    map.put("utente3", "utente3");
+    users = new HashMap<>();
+    users.put("admin", "admin");
+    users.put("utente1", "utente1");
+    users.put("utente2", "utente2");
+    users.put("utente3", "utente3");
 
     //sessioni
     sessions = new HashMap<>();
-    sessions.put("admin", null);
-    sessions.put("utente1", null);
-    sessions.put("utente2", null);
-    sessions.put("utente3", null);
+    sessions.put("admin", Optional.empty());
+    sessions.put("utente1", Optional.empty());
+    sessions.put("utente2", Optional.empty());
+    sessions.put("utente3", Optional.empty());
+
+    //gruppi
+    groups=new HashMap<>();
+    groups.put("admin", "g0");
+    groups.put("utente1", "g1");
+    groups.put("utente2", "g2");
+    groups.put("utente3", "g3");
 }
 
-    public Map<String, HttpSession> getSessions() {
+    public Map<String, Optional<HttpSession>> getSessions() {
         return sessions;
     }
 
-    public Map<String, String> getMap() {
-        return map;
+    public Map<String, String> getUsers() {
+        return users;
+    }
+
+    public Map<String, String> getGroups() {
+        return groups;
     }
 
 }
