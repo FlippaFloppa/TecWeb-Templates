9b8960c97f625a40ae998a493b5455d8474f5d8f
Patrick Di Fazio <50186694+BlessedRebuS@users.noreply.github.com>
2021-12-16 16:02:48 +0100

    Add files via upload

--- TemplatesLogin/src/servlets/Login.java
+++ TemplatesLogin/src/servlets/Login.java
@@ -1,29 +1,89 @@
 package servlets;
 
-import javax.servlet.ServletContext;
+import bean.Dati;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.io.PrintWriter;
-import bean.Dati;
+import java.util.Optional;
 
 
 public class Login extends HttpServlet {
     private static final long serialVersionUID = 1L;
+    private boolean checkLogin = false;
+    private boolean checkGroup = false;
+
+    private Dati d;
+
     @Override
-    public void init() {
+    public void init(ServletConfig conf) throws ServletException {
+        super.init(conf);
+        //dati con scope application
+        d = (Dati) getServletContext().getAttribute("dati");
 
     }
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
 
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
 
-        getServletContext().getRequestDispatcher("/carrello.jsp").forward(req,resp);
+        String r = req.getParameter("req");
 
+        if (r.equals("login")) {
+            checkLogin = checkLogin(username, password, req.getSession());
+            if (checkLogin) {
+                getServletContext().getRequestDispatcher("/carrello.jsp").forward(req, resp);
+                System.out.println("Ricevuto login da Username: " + username + " Password: " + password);
+            } else {
+                getServletContext().getRequestDispatcher("/errors/loginFailure.jsp?err=login").forward(req, resp);
+
+            }
+        }
+        if (r.equals("registration")) {
+            String group = req.getParameter("group");
+            checkGroup = registration(username, password, group, req.getSession());
+            if (checkGroup) {
+                getServletContext().getRequestDispatcher("/login.jsp").forward(req, resp);
+                System.out.println("Ricevuta registrazione da Username: " + username + " Password: " + password);
+            } else {
+                getServletContext().getRequestDispatcher("/errors/loginFailure.jsp?err=registration").forward(req, resp);
+
+            }
+        }
+
+    }
+
+    public boolean registration(String username, String password, String group, HttpSession session) {
+        boolean check = false;
+        //controllo che il gruppo sia tra quelli disponibili
+        if (group.equals("g0") || group.equals("g1") || group.equals("g2") || group.equals("g0")) {
+            d.getUsers().put(username, password);
+            d.getSessions().put(username, Optional.of(session));
+            d.getGroups().put(username, group);
+            System.out.println("Registrato utente: " + username + ", gruppo: " + group);
+            check = true;
+        }
+        return check;
+    }
+
+    public boolean checkLogin(String username, String password, HttpSession s) {
+        boolean check = false;
+        //controllo che nome e password corrispondano
+        if (d.getUsers().containsKey(username) && d.getUsers().get(username).equals(password)) {
+            //controllo che non ci sia la sessione per quell'utente
+            if (d.getSessions().get(username).isEmpty()) {
+                System.out.println("Loggato utente: " + username);
+                d.getSessions().put(username, Optional.of(s));
+                check = true;
+            }
+        }
+        return check;
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response)
