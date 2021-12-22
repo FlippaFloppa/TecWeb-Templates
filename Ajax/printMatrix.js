ddd47046220abcd6664c5957200c23aa0bb28bc3
Lorenzo Venerandi <68255980+Lore09@users.noreply.github.com>
2021-12-22 22:52:43 +0100

    Update printMatrix

--- Ajax/printMatrix.js
+++ Ajax/printMatrix.js
@@ -1,6 +1,7 @@
 //dimensione matrice
 var x=4; 
 var y=4;
+var matrix;
 
 function printMatrix(){
     
@@ -15,6 +16,27 @@ function printMatrix(){
         }
         div.innerHTML+='<br>';
     }
+}
+
+function printMatrixTable(){
+
+    //nel div matrix stampo le text area
+    var div=myGetElementById('matrix');
+    //var str='<input type="text" id="'+i+j+'" value="1" onchange="calcola()">';
+    var tab="";
+
+    tab+='<table>';
+
+    for(var i=0; i<x; i++){
+        tab+='<tr>';
+        for(var j=0; j<y; j++){
+            tab+='<td id="'+i+'-'+j+'">'+matrix[i][j]+'</td>';
+        }
+        tab+='</tr>';
+    }
+
+    tab+='</table>';
+    div.innerHTML=tab;
 }
 
 
@@ -33,4 +55,18 @@ function calcola(){
     alert(res);
     request("http://localhost:8080/TW_Esame_201223_2/calcola?matrix="+res, myGetElementById('res'));
 	//in res salvo il risultato
+}
+
+function getRandomMatrix(){
+
+    console.log("stampo matrice");
+
+    matrix = [];
+    for(let i=0;i<x;i++){
+        matrix[i]=[];
+        for(let j=0;j<y;j++){
+            matrix[i][j]=(Math.random() * 100) - 50;
+        }
+    }
+
 }
